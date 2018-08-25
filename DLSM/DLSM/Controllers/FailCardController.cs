288ab90b9aaf2095b67300c684b138be17e6d270
Narong Sungkhamalai <narong.skml@gmail.com>
2018-08-25 16:41:21 +0000

    Update FailCardController.cs

--- DLSM/DLSM/Controllers/FailCardController.cs
+++ DLSM/DLSM/Controllers/FailCardController.cs
@@ -30,36 +30,18 @@ namespace DLSM.Controllers
             return View();
         }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-        public ActionResult SearchCard(string searchCardNo
-                                   , string searchToCardNo
-                                   , DateTime? searchBeginDate
-                                   , DateTime? searchEndDate)
-=======
-        
         public ActionResult SearchCard(string searchCardNo
                                     , string searchToCardNo
                                     , DateTime? searchBeginDate
                                     , DateTime? searchEndDate)
->>>>>>> 1-failcard-create-filter-optional-optional
-=======
-
-        public ActionResult SearchCard(string searchCardNo
-                                     , string searchToCardNo
-                                     , DateTime? searchBeginDate
-                                     , DateTime? searchEndDate)
->>>>>>> 1-failcard-create-filter-optional-optional
         {
             var context = new DLSMEntities();
             int Userid = Convert.ToInt32(Session["UserID"]);
             List<StockSerial> list = new List<StockSerial>();
-<<<<<<< HEAD
-            var listdata = context.sp_SearchCard(Convert.ToString(searchCardNo), Convert.ToString(searchToCardNo), searchBeginDate, searchEndDate).ToList().OrderBy(a => a.id);
-=======
+
             var listdata = context.sp_SearchCard(Convert.ToString(searchCardNo), Convert.ToString(searchToCardNo), searchBeginDate, searchEndDate).ToList().OrderBy(a=>a.id);
->>>>>>> 1-failcard-create-filter-optional-optional
+
             if (listdata.Count() > 0)
             {
                 foreach (var i in listdata)
