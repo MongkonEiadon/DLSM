debe067235b7fe3c0e2e903d7a613a6a452a3b33
aek <ekarin.codizm@gmail.com>
2018-08-29 20:13:41 +0000

    .

--- DLSM/DLSM/Controllers/TransfersController.cs
+++ DLSM/DLSM/Controllers/TransfersController.cs
@@ -459,7 +459,7 @@ namespace DLSM.Controllers
                     {
                         var ddBegin = long.Parse(docdet.SerialBegin);
                         var ddEnd = long.Parse(docdet.SerialEnd);
-                        var objExist = ss_grp.SingleOrDefault(a => a.PdID == docdet.PdID && ddBegin >= a.SerialBegin && ddEnd <= a.SerialEnd);
+                         var objExist = ss_grp.SingleOrDefault(a => a.PdID == docdet.PdID && a.SerialBegin<=ddBegin &&  a.SerialEnd>= ddEnd);
                         if (objExist == null)
                         {
                             strError = docdet.Product.Name + " serial ที่ระบุไม่มีอยู่ในคลัง";
